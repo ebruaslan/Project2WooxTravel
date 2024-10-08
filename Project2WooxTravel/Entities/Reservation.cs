0326fef6ff2a4c9fe35e5d267520abf8809c10c2
Ebru ASLAN <ebruaslan.1453@gmail.com>
2024-10-08 21:39:19 +0300

    Add Admin Area

--- Project2WooxTravel/Entities/Reservation.cs
+++ Project2WooxTravel/Entities/Reservation.cs
@@ -9,7 +9,6 @@ namespace Project2WooxTravel.Entities
     {
         public int ReservationId { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; }
         public string Phone { get; set; }
         public int PersonCount { get; set; }
         public DateTime ReservationDate { get; set; }
---
+++ Project2WooxTravel/Migrations/202410081831542_mig11.cs
@@ -0,0 +1,18 @@
+﻿namespace Project2WooxTravel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig11 : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Reservations", "Email");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Reservations", "Email", c => c.String());
+        }
+    }
+}
