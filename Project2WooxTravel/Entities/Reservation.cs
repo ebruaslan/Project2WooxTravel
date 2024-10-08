f7ee72063d039fe537e489e3b00118d4807e5984
Ebru ASLAN <ebruaslan.1453@gmail.com>
2024-10-08 21:30:35 +0300

    Check

--- Project2WooxTravel/Entities/Reservation.cs
+++ Project2WooxTravel/Entities/Reservation.cs
@@ -8,6 +8,11 @@ namespace Project2WooxTravel.Entities
     public class Reservation
     {
         public int ReservationId { get; set; }
-        //public string ReservationId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int PersonCount { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public string Description { get; set; }
     }
 }
