70dac9ff5c6605f7b032aa55c9b1228f3a79d049
Ebru ASLAN <ebruaslan.1453@gmail.com>
2024-10-08 20:41:14 +0300

    Add project files.

---
+++ Project2WooxTravel/Context/TravelContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Project2WooxTravel.Entities;
+
+namespace Project2WooxTravel.Context
+{
+    public class TravelContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+    }
+}
---
+++ Project2WooxTravel/Entities/Reservation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project2WooxTravel.Entities
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        //public string ReservationId { get; set; }
+    }
+}
